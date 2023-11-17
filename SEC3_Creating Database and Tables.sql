a8b8279c92c163c11d37aa65d49bc47f262fe43a
Deepika11111 <151179663+Deepika11111@users.noreply.github.com>
2023-11-17 21:11:38 +0530

    Add files via upload

---
+++ SEC3_Creating Database and Tables.sql
@@ -0,0 +1,13 @@
+-- USE pet_shop; 
+-- CREATE TABLE Pet ( name varchar(20), Age int );
+-- Show Tables;
+-- Show columns from Pet; 
+-- Describe pet;
+-- Drop table pet; 
+-- Use Command+/ (forward Slash to comment down the code
+
+-- Home Work
+Create database Food;
+Use Food;
+Create table pastries ( name varchar(50), Quantity int); 
+Describe Food; ; 
